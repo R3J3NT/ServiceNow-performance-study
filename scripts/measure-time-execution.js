1bb831ef67b32cff4506ff67e1af773f27fa4fbd
Jakub <13350956+R3J3NT@users.noreply.github.com>
2021-12-22 20:36:08 +0100

    Create basic time measurement code for one script

--- scripts/measure-time-execution.js
+++ scripts/measure-time-execution.js
@@ -1,1 +1,10 @@
+//Script which can be executed as a Background Script to measure time of execution of specified script
 
+var timerStartTime = new Date().getTime(); //Start time of Script execution
+
+// Put your code here
+
+var timerEndTime = new Date().getTime(); //End time of Script execution
+var elapsedTimeMs = timerEndTime - timerStartTime; //Calcuate elapsed time by subtract starting value from final value
+
+gs.info('[Time Measure] - Time of script execution: ' + elapsedTimeMs + 'ms.'); //Log message with final execution time
