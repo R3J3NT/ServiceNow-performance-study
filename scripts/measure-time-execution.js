c241d54b6aecf2b7909b60d47d74177af6e29d80
Jakub <13350956+R3J3NT@users.noreply.github.com>
2021-12-22 21:08:39 +0100

    Introduce possibility of executing script multiply iterations and basic metrics calculation AVG,MAX,MIN

--- scripts/measure-time-execution.js
+++ scripts/measure-time-execution.js
@@ -1,10 +1,37 @@
 //Script which can be executed as a Background Script to measure time of execution of specified script
 
-var timerStartTime = new Date().getTime(); //Start time of Script execution
+var ITERATIONS = 100; //Number of iterations that script should be executed for better avg time results
 
-// Put your code here
+var executionTimePerIteration = []; // Array to keep execution time of every iteration
+var timerStartTime, timerEndTime, elapsedTimeMs = null; // Initialize timer variables with null value
 
-var timerEndTime = new Date().getTime(); //End time of Script execution
-var elapsedTimeMs = timerEndTime - timerStartTime; //Calcuate elapsed time by subtract starting value from final value
+for (var i = 0; i < ITERATIONS; i++) {
+    timerStartTime = new Date(); //Start time of Script execution
 
-gs.info('[Time Measure] - Time of script execution: ' + elapsedTimeMs + 'ms.'); //Log message with final execution time
+    //-------------------
+    // Put your code here
+    //-------------------
+
+    timerEndTime = new Date(); //End time of Script execution
+    elapsedTimeMs = timerEndTime - timerStartTime; //Calculate elapsed time by subtract starting value from final value
+    executionTimePerIteration.push(elapsedTimeMs);
+}
+
+var totalElapsedTime = executionTimePerIteration.reduce(function(acc, val) {
+    return acc + val;
+}, 0);
+
+var max = executionTimePerIteration.reduce(function(a, b) {
+    return Math.max(a, b);
+});
+
+var min = executionTimePerIteration.reduce(function(a, b) {
+    return Math.min(a, b);
+});
+
+//Log message with final execution time and statistics
+gs.info('[Time Measure] - Array with all iteration times: ' + executionTimePerIteration.toString());
+gs.info('[Time Measure] - Execution time of all iterations: ' + totalElapsedTime + 'ms.');
+gs.info('[Time Measure] - Average execution time of one iteration: ' + totalElapsedTime / ITERATIONS + 'ms.');
+gs.info('[Time Measure] - MAX execution time of iteration: ' + max + 'ms.');
+gs.info('[Time Measure] - MIN execution time of iteration: ' + min + 'ms.');
